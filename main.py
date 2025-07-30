29504b425cd327138761789549fb6267a5ed3315
Codyddr99 <davide.daronco@gmail.com>
2025-07-30 15:32:00 +0200

    main.py decluttering

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -21,9 +26,14 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
-        player.update(dt)
+        
+        updateable.update(dt)
+        
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
